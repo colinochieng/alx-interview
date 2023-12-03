874255e224e144d4da5bfb4dd204a748d3eea8fb
colinochieng <colinokumu89@gmail.com>
2023-12-04 02:17:59 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,78 +22,16 @@ def island_perimeter(grid):
                 the water surrounding the island).
     return: perimeter of island
     """
-    # strip: a row of land``
+    width = len(grid[0])
+    height = len(grid)
     perimeter = 0
 
-    for index, strip in enumerate(grid):
-        # grid is completely surrounded by water (remove outside rows)
-        if index > 0 and index < len(grid) - 1:
-            for idx, unit in enumerate(strip):
-                prev_strip = grid[index - 1]
-                next_strip = grid[index + 1]
-
-                # grid is completely surrounded by water
-                # (remove outside columns)
-                if idx > 0 and idx < len(strip) - 1:
-                    if (unit == 1 and strip[idx - 1] == 0
-                            and strip[idx + 1] == 0):
-                        # prev and next strip
-                        if prev_strip[idx] == 0 and next_strip[idx] == 0:
-                            # island with one sq grid
-                            return 4
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 0:
-                            perimeter += 3
-
-                        elif prev_strip[idx] == 0 and next_strip[idx] == 1:
-                            perimeter += 3
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 1:
-                            perimeter += 2
-
-                    elif (unit == 1 and strip[idx - 1] == 1
-                            and strip[idx + 1] == 0):
-                        # prev and next strip
-                        if prev_strip[idx] == 0 and next_strip[idx] == 0:
-                            perimeter += 3
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 0:
-                            perimeter += 2
-
-                        elif prev_strip[idx] == 0 and next_strip[idx] == 1:
-                            perimeter += 2
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 1:
-                            perimeter += 1
-
-                    elif (unit == 1 and strip[idx - 1] == 0
-                            and strip[idx + 1] == 1):
-                        # prev and next strip
-                        if prev_strip[idx] == 0 and next_strip[idx] == 0:
-                            perimeter += 3
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 0:
-                            perimeter += 2
-
-                        elif prev_strip[idx] == 0 and next_strip[idx] == 1:
-                            perimeter += 2
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 1:
-                            perimeter += 1
-
-                    elif (unit == 1 and strip[idx - 1] == 1
-                            and strip[idx + 1] == 1):
-                        # prev and next strip
-                        if prev_strip[idx] == 0 and next_strip[idx] == 0:
-                            perimeter += 2
-
-                        elif prev_strip[idx] == 1 and next_strip[idx] == 0:
-                            perimeter += 1
-
-                        elif prev_strip[idx] == 0 and next_strip[idx] == 1:
-                            perimeter += 1
-
-                        else:
-                            # enclosed cell: no perimeter to sum up
-                            continue
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
